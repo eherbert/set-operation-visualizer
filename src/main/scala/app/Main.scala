e2a17ebf7bf38aa13017ae4f71262ac39698d265
eherbert <emilyherbert@cs.umass.edu>
2018-11-13 16:06:00 -0500

    GUI changes and implement set names

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -16,6 +16,10 @@ import scala.collection.mutable.Buffer
 import scalafx.animation.AnimationTimer
 import scalafx.scene.input.KeyCode
 import scalafx.scene.input.KeyEvent
+import scalafx.scene.control.TextArea
+import scalafx.scene.layout.HBox
+import scalafx.scene.control.TextField
+import scalafx.scene.text.Text
 
 object Main extends JFXApp {
   val sceneWidth = 1200
@@ -24,25 +28,96 @@ object Main extends JFXApp {
   val sets = Buffer[MySet]()
   var selected = Buffer[MySet]()
 
+  
+  val newSetName = new TextArea()
+  newSetName.promptText = "Set Name"
+  newSetName.maxWidth = 125
+  newSetName.maxHeight = 25
   val newSetButton = new Button("New Set")
+  newSetButton.prefHeight = newSetName.height()
+  val newSetMessage = new Text()
   newSetButton.onAction = (e: ActionEvent) => {
-    sets += MySet(100, 100, 100)
+    val name = newSetName.text.getValue.trim()
+    if(name.length == 0) newSetMessage.text = "Set name cannot be empty."
+    else if(name.length > 11) newSetMessage.text = "Set name cannot be longer than 10 characters."
+    else if(name.filterNot(x => x.isLetter || x.isDigit).length > 0) newSetMessage.text = "Set name cannot contain non alpha-numeric characters."
+    else if(sets.map(_.name).contains(name)) newSetMessage.text = "Set name cannot already be in use."
+    else if(sets.length == 10) newSetMessage.text = "Only 10 sets allowed for simultaneous use."
+    else {
+      newSetMessage.text = "Set " + name + " created."
+      sets += MySet(name, 100, 100, 100)
+    }
   }
+  val topMenu = new HBox(10,newSetName,newSetButton,newSetMessage)
 
   val canvas = new Canvas(sceneWidth, sceneHeight)
   val gc = canvas.graphicsContext2D
 
+  var mouseX = 0.0
+  var mouseY = 0.0
   var prevMouseX = 0.0
   var prevMouseY = 0.0
-  var ctrlPressed = false
+  var deltaX = 0.0
+  var deltaY = 0.0
+
+  var mousePressed = false
+  var mouseDragged = false
+  var mouseReleased = false
+
+  canvas.onMousePressed = (e: MouseEvent) => {
+    mousePressed = true
+    mouseDragged = false
+    mouseReleased = false
+    prevMouseX = mouseX
+    prevMouseY = mouseY
+    mouseX = e.x
+    mouseY = e.y
+    deltaX = mouseX - prevMouseX
+    deltaY = mouseY - prevMouseY
+  }
+  canvas.onMouseDragged = (e: MouseEvent) => {
+    mousePressed = false
+    mouseDragged = true
+    mouseReleased = false
+    prevMouseX = mouseX
+    prevMouseY = mouseY
+    mouseX = e.x
+    mouseY = e.y
+    deltaX = mouseX - prevMouseX
+    deltaY = mouseY - prevMouseY
+  }
+  canvas.onMouseReleased = (e: MouseEvent) => {
+    mousePressed = false
+    mouseDragged = false
+    mouseReleased = true
+    prevMouseX = mouseX
+    prevMouseY = mouseY
+    mouseX = e.x
+    mouseY = e.y
+    deltaX = mouseX - prevMouseX
+    deltaY = mouseY - prevMouseY
+  }
+
+  def setMovementManagement(delta:Double) {
+    val viable = sets.filter(_.surrounds(mouseX, mouseY)) -- selected
+    if(viable.length > 0 && mousePressed) {
+      selected = Buffer(viable(0))
+      selected.foreach(_.selected())
+    }
+    else if(mouseDragged) selected.foreach(_ += (deltaX*delta*80, deltaY*delta*80))
+    else if(mouseReleased) {
+      selected.foreach(_.deselected())
+      selected.clear()
+    }
+  }
 
   stage = new PrimaryStage {
     title = "Set Operation Visualizer"
     resizable = false
     scene = new Scene(sceneWidth, sceneHeight) {
       val border = new BorderPane
-      border.left = newSetButton
       border.center = canvas
+      border.top = topMenu
 
       root = border
 
@@ -54,41 +129,7 @@ object Main extends JFXApp {
           gc.clearRect(0, 0, canvas.width(), canvas.height())
           sets.foreach(_.render(gc))
 
-          onKeyPressed = (e: KeyEvent) => {
-            e.code match {
-              case KeyCode.CONTROL => ctrlPressed = true
-              case _ =>
-            }
-          }
-
-          onKeyReleased = (e: KeyEvent) => {
-            e.code match {
-              case KeyCode.CONTROL => ctrlPressed = false
-              case _ =>
-            }
-          }
-
-          canvas.onMousePressed = (e: MouseEvent) => {
-            val viable = sets.filter(_.surrounds(e.x, e.y)) -- selected
-            if (viable.length > 0) {
-              if (ctrlPressed) selected += viable(0)
-              else selected = Buffer(viable(0))
-            }
-            selected.foreach(_.selected())
-            prevMouseX = e.x
-            prevMouseY = e.y
-          }
-          canvas.onMouseDragged = (e: MouseEvent) => {
-            selected.foreach(_ += (e.x - prevMouseX, e.y - prevMouseY))
-            prevMouseX = e.x
-            prevMouseY = e.y
-          }
-          canvas.onMouseReleased = (e: MouseEvent) => {
-            if (!ctrlPressed) {
-              selected.foreach(_.deselected())
-              selected.clear()
-            }
-          }
+          setMovementManagement(delta)
         }
         lastTime = t
       })
--- src/main/scala/app/MySet.scala
+++ src/main/scala/app/MySet.scala
@@ -4,42 +4,48 @@ import scalafx.scene.canvas.GraphicsContext
 import scalafx.scene.paint.Color
 
 class MySet private (
-  name: String,
+  val name: String,
   var x: Double,
   var y: Double,
   var r: Double) {
   
   var strokeColor = Color.BLACK
+  var fillColor = Color.BLACK
 
+  private def cx():Double = x+r
+  private def cy():Double = y+r
+  
   def += (a: Double, b:Double) {
     x += a
     y += b
   }
   
   def surrounds(a: Double, b: Double): Boolean = {
-    val c = math.sqrt(math.pow(a - (x+r), 2) + math.pow(b - (y+r), 2))
+    val c = math.sqrt(math.pow(a - cx(), 2) + math.pow(b - cy(), 2))
     c < r
   }
 
   def selected() {
     strokeColor = Color.GREEN
+    fillColor = Color.GREEN
   }
   
   def deselected() {
     strokeColor = Color.BLACK
+    fillColor = Color.BLACK
   }
   
   def render(gc: GraphicsContext) {
     gc.stroke = strokeColor
     gc.strokeOval(x, y, r*2, r*2)
+    gc.fill = fillColor
+    gc.fillText(name, x, y)
   }
 }
 
 object MySet {
-  var nextName = "A"
-
-  def apply(x: Double, y: Double, r:Double) = {
-    val set = new MySet(nextName, x, y, r)
-    set
+  
+  def apply(name:String, x: Double, y: Double, r:Double) = {
+    new MySet(name, x, y, r)
   }
 }
